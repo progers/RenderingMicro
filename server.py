c8301850a9c1410028c8bc8b3414dfcc4c0dabb0
Philip Rogers <pdr@chromium.org>
2024-12-30 13:51:30 -0800

    Prevent caching in server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-# Create a simple server which sends cross-origin isolated headers, which are
-# required for timer precision, see:
+# Create a simple no-caching server which sends cross-origin isolated headers,
+# which are required for timer precision, see:
 # https://developer.mozilla.org/en-US/docs/Web/API/Window/crossOriginIsolated
 #
 # Run `python3 server.py`, then open http://localhost:8000/benchmark.html
@@ -12,6 +12,8 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
         self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+        # Prevent caching
+        self.send_header('Cache-Control', 'no-cache, no-store, must-revalidate')
         SimpleHTTPRequestHandler.end_headers(self)
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
